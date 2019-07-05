92b300966defaf39bf35917175426ac4eff40b91
dnzlike <dnz7993@sjtu.edu.cn>
2019-07-05 19:46:58 +0800

    finish lock

--- myapp/src/main/java/Core/LockService.java
+++ myapp/src/main/java/Core/LockService.java
@@ -1,22 +1,23 @@
 package Core;
 
 import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
-import java.io.IOException;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 class LockWatcher implements Watcher {
-    CountDownLatch latch;
+    private CountDownLatch latch;
     LockWatcher(CountDownLatch countDownLatch){
         this.latch = countDownLatch;
     }
 
     @Override
     public void process(WatchedEvent event) {
-        if(event.getType()== Event.EventType.NodeDeleted){//当前节点是否删除
+        if(event.getType()== Event.EventType.NodeDeleted){
+            System.out.println(event.getPath().substring(6) + " is released");
             latch.countDown();
         }
     }
@@ -26,15 +27,20 @@ public class LockService {
     private static ZooKeeper zookeeper;
     private static String zookeeper_server;
     private static String parentPath = "/lock";
-    static CountDownLatch latch = new CountDownLatch(1);
+    private static String lockPrefix = "/lock/lock-";
+    private static String lockPath;
+    private static CountDownLatch latch = new CountDownLatch(1);
+    private static LockWatcher lockWatcher = new LockWatcher(latch);
 
     static public void init() {
         zookeeper_server = new String("dist-1:2181,dist-2:2181,dist-3:2181");
 
-
         try {
-            zookeeper = new ZooKeeper("localhost:2181",2000,null);
-//            zookeeper.create(parentPath, "for lock".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zookeeper = new ZooKeeper(zookeeper_server,2000, lockWatcher);
+            Stat stat = zookeeper.exists(parentPath, null);
+            if (stat == null) {
+                zookeeper.create(parentPath, "for lock".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -42,37 +48,43 @@ public class LockService {
 
     public static void lock(){
         try {
-            String lockPath = "/lock/lock-";
-            String nodePath = zookeeper.create(lockPath, "lock node".getBytes(),
+            lockPath = zookeeper.create(lockPrefix, "lock node".getBytes(),
                     ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            String lockId = nodePath.substring(lockPath.length()); // 0000000001
-            nodePath = nodePath.substring(parentPath.length() + 1); // lock-0000000001
+            String lockId = lockPath.substring(lockPrefix.length()); // 0000000001
+            String nodePath = lockPath.substring(parentPath.length() + 1); // lock-0000000001
 
             List<String> children = zookeeper.getChildren("/lock", false);
-            children.add("lock-0000000012");
             Collections.sort(children, new Comparator<String>() {
                 public int compare(String left, String right) {
-                    String leftId = left.substring(lockPath.length());
-                    String rightId = right.substring(lockPath.length());
+                    String leftId = left.substring(lockPrefix.length());
+                    String rightId = right.substring(lockPrefix.length());
                     return leftId.compareTo(rightId);
                 }
             });
             if (nodePath.equals(children.get(0))) {
-                System.out.println(nodePath + " get the lock");
+                System.out.println(nodePath + " acquire the lock");
             }
             else {
                 Integer preValue = Integer.valueOf(lockId) - 1;
                 String preId = String.format("%010d", preValue);
-                String prePath = lockPath + preId;
-                System.out.println(prePath);
+                String prePath = lockPrefix + preId;
+                System.out.println(lockPath.substring(parentPath.length() + 1) +
+                        " is watching " + prePath.substring(parentPath.length() + 1));
+                zookeeper.exists(prePath, lockWatcher);
+                latch.await();
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     public static void unlock() {
-
+        try {
+            System.out.println(lockPath.substring(parentPath.length() + 1) + " release the lock");
+            latch = new CountDownLatch(1);
+            zookeeper.delete(lockPath, -1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
