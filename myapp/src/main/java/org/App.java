fb31eb71c120fc4179618beb8b0d599eefa627c2
huashui <708846441@qq.com>
2019-07-08 18:09:55 +0800

    huashui

--- myapp/src/main/java/org/App.java
+++ myapp/src/main/java/org/App.java
@@ -1,4 +1,4 @@
-package org;
+package myapp.src.main.java.org;
 
 
 import Core.Current;
@@ -7,9 +7,14 @@ import org.apache.spark.*;
 import org.apache.spark.streaming.*;
 import org.apache.spark.streaming.dstream.*;
 import org.apache.spark.streaming.kafka.KafkaUtils;
+
+import java.sql.Connection;
 import java.util.HashMap;
+
 import org.apache.spark.streaming.kafka.*;
+
 import java.util.Map;
+
 import org.apache.spark.streaming.api.java.JavaPairReceiverInputDStream;
 import org.apache.spark.*;
 import org.apache.spark.api.java.function.*;
@@ -17,20 +22,24 @@ import org.apache.spark.streaming.*;
 import org.apache.spark.streaming.api.java.*;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
+
 import java.util.Arrays;
 import java.util.regex.Pattern;
+
 import org.OrderProcessor;
 
+import javax.activation.DataSource;
 
-public class App{
 
-    static void init(){
+public class App {
+
+    static void init() {
         OrderProcessor.init();
         Current.connectZookeeper();
         LockService.init();
     }
 
-    public static void main(String args[])throws Exception {
+    public static void main(String args[]) throws Exception {
         System.out.println("hello world");
 
         SparkConf conf = new SparkConf().setAppName("Order Processing");
@@ -40,16 +49,37 @@ public class App{
         JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("WARN");
         JavaStreamingContext jssc = new JavaStreamingContext(sc, Durations.seconds(2));
-        Map<String, Integer> topicMap = new HashMap<String,Integer>();
-        topicMap.put("kafka_spark",1);
+        Map<String, Integer> topicMap = new HashMap<String, Integer>();
+        topicMap.put("kafka_spark", 1);
         JavaPairReceiverInputDStream<String, String> messages =
-                KafkaUtils.createStream(jssc,
+                KafkaUtils.createDirectStream(jssc,
                         "dist-1:2181,dist-2:2181,dist-3:2181", "spark_receiver", topicMap);
-
-        JavaDStream<String> lines = messages.map(Tuple2::_2);
-
+        final AtomicReference<OffsetRange[]> offsetRanges = new AtomicReference<>();
+//        JavaDStream<String> lines = messages.map(Tuple2::_2);
+        JavaDStream<String> lines = messages.transformToPair(
+                new Function<JavaPairRDD<String, String>, JavaPairRDD<String, String>>() {
+                    @Override
+                    public JavaPairRDD<String, String> call(JavaPairRDD<String, String> rdd) throws Exception {
+                        OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
+                        offsetRanges.set(offsets);
+                        return rdd;
+                    }
+                }
+        ).map(Tuple2::_2);
         //JavaDStream<String> words = lines.flatMap(x -> Arrays.asList(Pattern.compile(" ").split(x)).iterator());
         JavaDStream<String> results = lines.map(OrderProcessor::process);
+        messages.foreachRDD(new VoidFunction<JavaPairRDD<String, String>>() {
+            @Override
+            public void call(JavaPairRDD<String, String> t) throws Exception {
+                for (OffsetRange offsetRange : offsetRanges.get()) {
+                    System.out.println("update kafka_offsets set offset ='"
+                            + offsetRange.untilOffset() + "'  where topic='"
+                            + offsetRange.topic() + "' and partition='"
+                            + offsetRange.partition() + "'");
+                }
+            }
+
+        });
         //JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey((i1, i2) -> (i1 + i2));
         System.out.println("test");
         results.print();
